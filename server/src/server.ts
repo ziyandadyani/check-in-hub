6302e33a9c8b72fb88485df86ce239f87d2fca65
Ziyanda Dyani <ziyandadyani17@gmail.com>
2026-03-22 22:46:33 +0200

    Initialized project. Express and Database setup

---
+++ server/src/server.ts
@@ -0,0 +1,11 @@
+import * as appModule from './app';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = (appModule as any).default || appModule;
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
