f20bf369b0f655c463dd5f5ffce6d418a532437f
Ziyanda Dyani <ziyandadyani17@gmail.com>
2026-03-24 18:41:50 +0200

    connect frontend to deployed backend

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,7 +1,31 @@
+// import axios from 'axios';
+
+// const API = axios.create({
+//   baseURL: 'http://localhost:3000/api',
+// });
+
+// // GET venues
+// export const getVenues = () => API.get('/venues');
+
+// // POST clock-in
+// export const clockIn = (data: {
+//   venueId: string;
+//   latitude: number;
+//   longitude: number;
+// }) => API.post('/clock-in', data);
+
+// //GET clock-ins 
+// export const getClockIns = (venueId?: string) => {
+//   return API.get('/admin/clock-ins', {
+//     params: venueId ? { venueId } : {}
+//   });
+// };
+
 import axios from 'axios';
 
+// Get the backend URL from Vite env
 const API = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: import.meta.env.VITE_API_URL + '/api',
 });
 
 // GET venues
@@ -14,7 +38,7 @@ export const clockIn = (data: {
   longitude: number;
 }) => API.post('/clock-in', data);
 
-//GET clock-ins 
+// GET clock-ins 
 export const getClockIns = (venueId?: string) => {
   return API.get('/admin/clock-ins', {
     params: venueId ? { venueId } : {}
