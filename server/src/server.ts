c472125484521ca8c010b75f182881716eadc37a
Ziyanda Dyani <ziyandadyani17@gmail.com>
2026-03-24 18:14:39 +0200

    Added README file

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,7 +4,7 @@ dotenv.config();
 
 const app = (appModule as any).default || appModule;
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
