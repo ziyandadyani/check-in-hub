4aa3e3a8aa565d7987266fcaa5ef3c3a1fabc939
Ziyanda Dyani <ziyandadyani17@gmail.com>
2026-03-25 16:52:12 +0200

    updated README and formatted date

--- shared/types.ts
+++ shared/types.ts
@@ -20,8 +20,23 @@ export type ClockInRequest = {
   venueId: string;
   latitude: number;
   longitude: number;
+  userId: number   // added this 
 };
+
+// export type ClockInResponse = {
+//   message: string;
+// };
 
 export type ClockInResponse = {
   message: string;
+  clockIn?: {
+    id: string;
+    user_id: string;
+    venue_id: string;
+    latitude: number;
+    longitude: number;
+    distance: number;
+    created_at: string;
+  };
+  error?: any;
 };
