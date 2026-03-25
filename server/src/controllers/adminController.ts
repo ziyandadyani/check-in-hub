fb83887f72cc2989fac3f1537c32c04b6bc7a9ea
Ziyanda Dyani <ziyandadyani17@gmail.com>
2026-03-25 13:02:48 +0200

    corrected clock-in supabase query

--- server/src/controllers/adminController.ts
+++ server/src/controllers/adminController.ts
@@ -13,8 +13,8 @@ export const getClockIns = async (req: Request, res: Response) => {
         venue_id,
         distance,
         created_at,
-        user(name),
-        venue(name)
+        users(name),
+        venues(name)
       `);
 
     if (venueId) {
